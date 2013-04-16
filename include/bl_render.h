34be20d650226aeb112f4ff9785ff79974fe56ce
Sora <anthonin.bonnefoy@gmail.com>
2013-04-16 08:23:58 +0200

    Setup opengl initialization in render

--- include/bl_render.h
+++ include/bl_render.h
@@ -1,11 +1,18 @@
 #ifndef BL_RENDER_H
 #define BL_RENDER_H
 
+#include <GL/glew.h>
+#include <GL/gl.h>
+
 class BlRender {
         public:
                 BlRender(void);
+
+                void init(void);
                 void render(void);
+
         private:
+                GLuint vao;
 };
 
 #endif
--- src/bl_render.cpp
+++ src/bl_render.cpp
@@ -1,8 +1,18 @@
 #include <bl_render.h>
-#include <GL/gl.h>
 
 BlRender::BlRender()
 {
+}
+
+void BlRender::init()
+{
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glDepthFunc(GL_LESS);
+
 }
 
 void BlRender::render()
