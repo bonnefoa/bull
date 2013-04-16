4e6e0619ce09bb5f91f6e8d5b1e843014d847da1
Sora <anthonin.bonnefoy@gmail.com>
2013-04-16 08:21:08 +0200

    Adding shader program class

---
+++ include/bl_program.h
@@ -0,0 +1,18 @@
+#ifndef BL_PROGRAM_H
+#define BL_PROGRAM_H
+
+#include <bl_shader.h>
+#include <vector>
+
+class BlProgram
+{
+        public:
+                GLuint programId;
+                BlProgram(std::vector<BlShader *> shaders)
+                        : shaders(shaders) {programId=0;};
+                void loadProgram(void);
+        private:
+                std::vector<BlShader *> shaders;
+};
+
+#endif
---
+++ src/bl_program.cpp
@@ -0,0 +1,29 @@
+#include "bl_program.h"
+#include <stdio.h>
+#include <bl_util.h>
+
+void BlProgram::loadProgram()
+{
+        if (programId == 0) {
+                programId = glCreateProgram();
+                printf("Initialized program with id %d\n", programId);
+        } else {
+                for(unsigned int i = 0; i < shaders.size(); i++) {
+                        glDetachShader(programId, (*shaders[i]).shaderId);
+                }
+        }
+        GLint program_ok;
+        for(unsigned int i = 0; i < shaders.size(); i++){
+                GLuint shaderId = (*shaders[i]).loadShader();
+                glAttachShader(programId, shaderId);
+        }
+        glLinkProgram(programId);
+
+        glGetProgramiv(programId, GL_LINK_STATUS, &program_ok);
+        if(!program_ok)
+        {
+                fprintf(stderr, "Failed to link shader program:\n");
+                show_info_log(programId, glGetProgramiv, glGetProgramInfoLog);
+                glDeleteProgram(programId);
+        }
+}
