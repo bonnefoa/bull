28717eae6cb72b95ce8e375c34faeba641b51572
Sora <anthonin.bonnefoy@gmail.com>
2013-04-17 08:19:19 +0200

    Added forgotten load program for model program

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,24 @@ void main_loop()
         blInput->pollInput();
         blInput->handleInput();
         blRender.render();
-        SDL_GL_SwapWindow(blWindow.window);
 }
 
 void init()
 {
         blWindow.launch();
+        blRender.init();
         blInput = new BlInput();
 
         std::vector<BlShader*> shaders;
-        BlShader *modelVertexShader = new BlShader("glsl/model_vertex.glsl", GL_VERTEX_SHADER);
-        BlShader *modelFragmentShader = new BlShader("glsl/model_fragment.glsl", GL_FRAGMENT_SHADER);
+        BlShader *modelVertexShader = new BlShader("glsl/model_vertex.glsl"
+                        , GL_VERTEX_SHADER);
+        BlShader *modelFragmentShader = new BlShader("glsl/model_fragment.glsl"
+                        , GL_FRAGMENT_SHADER);
         shaders.push_back(modelVertexShader);
         shaders.push_back(modelFragmentShader);
 
         blProgramModel = new BlProgramModel(shaders, blInput);
+        blProgramModel->loadProgram();
         blProgramModel->init();
 }
 
@@ -42,6 +45,7 @@ int main()
                         blWindow.shutdown();
                         return 0;
                 }
+                SDL_GL_SwapWindow(blWindow.window);
         }
         return 1;
 }
