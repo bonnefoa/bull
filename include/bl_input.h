d5cd120c469efa6ae233b22ee807ffe22c23442f
Sora <anthonin.bonnefoy@gmail.com>
2013-04-19 08:19:18 +0200

    Added view matrix and projection matrix computation

--- include/bl_input.h
+++ include/bl_input.h
@@ -5,18 +5,37 @@
 
 class BlInput
 {
+        float phi;
+        float theta;
+        float mouseSpeed;
+        float speed;
+        float fov;
+        const float aspect;
+        const float zNear;
+        const float zFar;
+        double lastTicks;
+        bool keys[322];
+        btVector3 position;
 
         public:
-                bool keys[322];
-                int gameState;
-
                 BlInput();
                 void pollInput();
                 void handleInput();
 
-                btMatrix3x3 view;
-                btMatrix3x3 projection;
-};
+                int gameState;
 
+                btTransform view;
+                btTransform projection;
+        private:
+                btVector3 computeCurrentDirection();
+                void handleMovement();
+                float getDeltaTime();
+                void computeNewAngles(float deltaTime);
+                btTransform computeView(const btVector3 &lookAt
+                                , const btVector3 &right
+                                , const btVector3 &up
+                                , const btVector3 &position);
+                btTransform computeProjection();
+};
 
 #endif
--- src/bl_input.cpp
+++ src/bl_input.cpp
@@ -1,11 +1,19 @@
 #include <bl_input.h>
 #include <SDL.h>
 
-BlInput::BlInput()
+BlInput::BlInput(): aspect(4.0f/3.0f), zNear(0.1f), zFar(100.0f)
 {
         for(int i = 0; i < 322; i++) {
                 keys[i] = false;
         }
+        phi = 3.14f;
+        theta = 0.0f ;
+        fov = 45.0f;
+        speed = 8.0f ;
+        mouseSpeed = 0.1f ;
+        lastTicks = 0 ;
+        position = btVector3(0.f, 0.f, 0.f);
+        SDL_SetRelativeMouseMode(SDL_TRUE);
 }
 
 void BlInput::pollInput()
@@ -26,6 +34,77 @@ void BlInput::pollInput()
                                 break;
                 }
         }
+}
+
+btVector3 BlInput::computeCurrentDirection()
+{
+        btScalar x = sin(theta) * sin(phi);
+        btScalar y = cos(theta);
+        btScalar z = sin(theta) * cos(phi);
+        return btVector3(x, y, z);
+}
+
+float BlInput::getDeltaTime()
+{
+        double currentTicks = SDL_GetTicks();
+        float deltaTime = float(currentTicks - lastTicks);
+        lastTicks = currentTicks;
+        return deltaTime;
+}
+
+void BlInput::computeNewAngles(float deltaTime)
+{
+        int deltaX, deltaY;
+        SDL_GetRelativeMouseState(&deltaX, &deltaY);
+        phi += mouseSpeed * deltaTime * float(deltaX);
+        theta -= mouseSpeed * deltaTime * float(deltaY);
+        theta = fmax(0, theta);
+        theta = fmin(theta, 3.14);
+}
+
+btTransform BlInput::computeView(const btVector3 &lookAt
+                , const btVector3 &right
+                , const btVector3 &up, const btVector3 &position)
+{
+       btTransform transform;
+       btMatrix3x3 basis;
+       basis[0] = right;
+       basis[1] = up;
+       basis[2] = lookAt;
+       transform.setBasis(basis.transpose());
+       transform.setOrigin(position * btScalar(-1));
+       return transform;
+}
+
+btTransform BlInput::computeProjection()
+{
+       btTransform res;
+       return res;
+}
+
+void BlInput::handleMovement()
+{
+        btVector3 direction, up, right;
+        float deltaTime;
+
+        deltaTime = getDeltaTime();
+        computeNewAngles(deltaTime);
+
+        direction = computeCurrentDirection();
+        right = btVector3(sin(phi - 3.14f / 2.0f), 0, cos(phi - 3.14f / 2.0f));
+        up = right.cross(direction);
+
+        if(keys[SDLK_UP]) {
+                position += direction * deltaTime * speed;
+        } else if(keys[SDLK_DOWN]) {
+                position -= direction * deltaTime * speed;
+        } else if(keys[SDLK_LEFT]) {
+                position -= right * deltaTime * speed;
+        } else if(keys[SDLK_RIGHT]) {
+                position += right * deltaTime * speed;
+        }
+        view = computeView(direction, right, up, position);
+        projection = computeProjection();
 }
 
 void BlInput::handleInput()
