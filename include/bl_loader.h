f85d74db16518c2488df0e1c98753a5808340900
Sora <anthonin.bonnefoy@gmail.com>
2013-05-05 16:40:26 +0200

    Fix xml loading

--- include/bl_loader.h
+++ include/bl_loader.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <libxml/parser.h>
+#include <libxml/tree.h>
 #include <vector>
 #include <bl_model.h>
 
--- src/bl_loader.cpp
+++ src/bl_loader.cpp
@@ -4,17 +4,9 @@
 
 BlModel *loadModelNode(xmlNode *node)
 {
-        xmlNode *attrNode;
         BlModel *blModel;
-        xmlAttr *attr = node->properties;
-        for(attrNode = attr->children;
-                        attrNode;
-                        attrNode = attrNode->next) {
-                if(strcmp((char *)attrNode->name, "filename") == 0) {
-                         blModel = new BlModel(
-                                         (const char *)attrNode->content);
-                }
-        }
+        blModel = new BlModel((const char *)xmlGetProp(node, (const unsigned char *)"filename"));
+        blModel->loadAsset();
         return blModel;
 }
 
@@ -28,8 +20,6 @@ std::vector<BlModel*> *loadScene(char *filename)
         document = xmlReadFile(filename, NULL, 0);
         root = xmlDocGetRootElement(document);
         for (node = root->children; node; node = node->next) {
-                INFO("\t Child is <%s> (%i) %s\n",
-                                node->name, node->type, node->content);
                 if(strcmp((char *)node->name, "model") == 0) {
                         models->push_back(loadModelNode(node));
                 }
