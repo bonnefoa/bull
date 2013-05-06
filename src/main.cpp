e58d59796c3d4643d91b8bd5c5198cf6f94b9800
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-05-06 20:34:38 +0200

    Clear depth buffer

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ void logState()
 void mainLoop()
 {
         glClearColor( 0.0, 0.0, 0.2, 1.0 );
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         blInput->handleInput();
         logState();
 
