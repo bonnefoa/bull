73ffade4faac304c724e4d966d2ed74575747ff6
Sora <anthonin.bonnefoy@gmail.com>
2013-05-08 10:40:28 +0200

    Adding name and rigidBody to model

--- include/bl_model.h
+++ include/bl_model.h
@@ -8,14 +8,16 @@
 
 class BlModel {
         public:
-                BlModel(std::vector<btVector3> vertices
-                                , std::vector<unsigned int> indices
-                                , btVector3 position
-                                , float mass) :
-                        vertices(vertices)
-                        , indices(indices)
-                        , position(position)
-                        , mass(mass) {};
+                BlModel(std::vector<btVector3> verticesInput
+                                , std::vector<unsigned int> indicesInput
+                                , btVector3 positionInput
+                                , float massInput
+                                , const char *nameInput) :
+                        vertices(verticesInput)
+                        , indices(indicesInput)
+                        , position(positionInput)
+                        , mass(massInput)
+                        , name(nameInput) {};
                 void init();
                 void loadInBuffer();
                 bool loadAsset(void);
@@ -26,6 +28,8 @@ class BlModel {
                 std::vector <unsigned int> indices;
                 btVector3 position;
                 float mass;
+                const char *name;
+                btRigidBody *rigidBody;
 
         private:
                 GLuint vertexBuffer;
