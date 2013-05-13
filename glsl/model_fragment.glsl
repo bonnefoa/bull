7612b168db48d9ffbc9b482540d69ff39147d97e
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-05-13 13:59:06 +0200

    Extract some functions on fragment glsl

--- glsl/model_fragment.glsl
+++ glsl/model_fragment.glsl
@@ -18,15 +18,27 @@ in vec3 vertexPosition_worldspace;
 in vec3 lightDirection_worldspace;
 in vec3 lightPosition_worldspace;
 
+float getDiffuseCoefficient()
+{
+        float coef = max(dot(vertexNormal_worldspace
+                                , lightDirection_worldspace), 0.0);
+        coef = clamp(coef, 0.0, 1.0);
+        return coef;
+}
+
+float getAttenuation()
+{
+       float lightDistance = length(lightPosition_worldspace - vertexPosition_worldspace);
+       float attenuation = 1 / (lightConstantAttenuation + lightLinearAttenuation * lightDistance + lightQuadraticAttenuation * lightDistance * lightDistance);
+       return attenuation;
+}
+
 void main()
 {
        vec3 texColor = texture(textureSampler, UV).xyz;
-       float coef = max(dot(vertexNormal_worldspace
-                , lightDirection_worldspace), 0.0);
-       coef = clamp(coef, 0.0, 1.0);
        vec3 ambientPart = ambientColor * texColor;
-       float lightDistance = length(lightPosition_worldspace - vertexPosition_worldspace);
-       float attenuation = 1 / (lightConstantAttenuation + lightLinearAttenuation * lightDistance + lightQuadraticAttenuation * lightDistance * lightDistance);
+       float coef = getDiffuseCoefficient();
+       float attenuation = getAttenuation();
        vec3 diffusePart = texColor * lightColor * coef * attenuation;
        color = ambientPart + diffusePart;
 }
