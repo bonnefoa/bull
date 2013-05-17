78203e5c4e0a570633a0e3ca7f6afd1186a47204
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-05-17 12:20:21 +0200

    Added simple debug draw

--- include/bl_debug_drawer.h
+++ include/bl_debug_drawer.h
@@ -4,14 +4,19 @@
 #include <bullet.h>
 #include <GL/glew.h>
 #include <GL/gl.h>
+#include <vector>
 
 class BlDebugDrawer : public btIDebugDraw {
         public:
                 void init();
+                virtual void drawLine(const btVector3& from,const btVector3& to,const btVector3& color);
 
         private:
                 GLuint lineBuffer;
+                GLuint colorBuffer;
+
                 std::vector<btVector3> lines;
+                std::vector<btVector3> colors;
 };
 
 #endif
--- src/bl_debug_drawer.cpp
+++ src/bl_debug_drawer.cpp
@@ -3,9 +3,24 @@
 void BlDebugDrawer::init()
 {
         glGenBuffers(1, &lineBuffer);
+        glGenBuffers(1, &colorBuffer);
 }
 
-void BlDebugDrawer::btIDebugDraw::drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
+void BlDebugDrawer::drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
 {
-        glDrawElements(GL_LINE, indices.size(), GL_UNSIGNED_INT, (void *)0);
+        colors.clear();
+        colors.push_back(color);
+        colors.push_back(color);
+
+        lines.clear();
+        lines.push_back(from);
+        lines.push_back(to);
+
+        glBindBuffer(GL_COLOR_ARRAY, colorBuffer);
+        glBufferData(GL_COLOR_ARRAY, 6 * sizeof(GL_FLOAT), &colors[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ARRAY_BUFFER, lineBuffer);
+        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GL_FLOAT), &lines[0], GL_STATIC_DRAW);
+
+        glDrawArrays(GL_LINE, 0, 2);
 }
