dd14beba7a03be2ff39aa9d37a74d5fdfb4e21c0
Sora <anthonin.bonnefoy@gmail.com>
2013-06-03 16:05:28 +0200

    Added uv + texture for debug fragment

--- glsl/debug_fragment.glsl
+++ glsl/debug_fragment.glsl
@@ -1,9 +1,13 @@
 #version 130
 
+uniform sampler2D textureSampler;
+
 in vec3 fragmentColor;
+in vec2 UV;
+
 out vec3 color;
 
 void main()
 {
-        color = fragmentColor;
+        color = fragmentColor + texture(textureSampler, UV).xyz;
 }
--- glsl/debug_vertex.glsl
+++ glsl/debug_vertex.glsl
@@ -5,11 +5,14 @@ uniform mat4 P;
 
 in vec3 vertexPosition_modelspace;
 in vec3 vertexColor;
+in vec2 vertexUV;
 
 out vec3 fragmentColor;
+out vec2 UV;
 
 void main()
 {
         gl_Position = P * V * vec4(vertexPosition_modelspace, 1.0f);
         fragmentColor = vertexColor;
+        UV = vertexUV;
 }
