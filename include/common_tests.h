43514ab717ab7541d55e69905ba5ce49a97fefb8
Sora <anthonin.bonnefoy@gmail.com>
2013-06-11 18:20:58 +0200

    Better vector assertion

--- include/common_tests.h
+++ include/common_tests.h
@@ -2,7 +2,11 @@
 #define assert_float_equals(res, expected) \
         fail_unless(fabs(res - expected) < EPSILON\
                         , "Expected %f, got %f", expected, res)
+
 #define assert_vector_equals(first, second) \
-        assert_float_equals(first[0],second[0]); \
-        assert_float_equals(first[1],second[1]); \
-        assert_float_equals(first[2],second[2]);
+        fail_unless(fabs(first[0] - second[0]) < EPSILON \
+                        && fabs(first[1] - second[1]) < EPSILON \
+                        && fabs(first[2] - second[2]) < EPSILON \
+                        , "Expected [%f,%f,%f], got [%f,%f,%f]", \
+                        first[0], first[1], first[2], \
+                        second[0], second[1], second[2])
