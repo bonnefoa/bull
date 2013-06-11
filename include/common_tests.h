299aac7221097288b4f14756b71cbbb3e09d2c0e
Sora <anthonin.bonnefoy@gmail.com>
2013-06-11 18:14:10 +0200

    Added common assert for vector equality

--- include/common_tests.h
+++ include/common_tests.h
@@ -2,3 +2,7 @@
 #define assert_float_equals(res, expected) \
         fail_unless(fabs(res - expected) < EPSILON\
                         , "Expected %f, got %f", expected, res)
+#define assert_vector_equals(first, second) \
+        assert_float_equals(first[0],second[0]); \
+        assert_float_equals(first[1],second[1]); \
+        assert_float_equals(first[2],second[2]);
