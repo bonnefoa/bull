9fe123801ef6536c8c9e12a9306b1f929768e694
Sora <anthonin.bonnefoy@gmail.com>
2013-06-11 19:19:17 +0200

    Added assertion for quaternion

--- include/common_tests.h
+++ include/common_tests.h
@@ -10,3 +10,13 @@
                         , "Expected [%f,%f,%f], got [%f,%f,%f]", \
                         first[0], first[1], first[2], \
                         second[0], second[1], second[2])
+
+
+#define assert_quaternion_equals(first, second) \
+        fail_unless(fabs(first[0] - second[0]) < EPSILON \
+                        && fabs(first[1] - second[1]) < EPSILON \
+                        && fabs(first[2] - second[2]) < EPSILON \
+                        && fabs(first[3] - second[3]) < EPSILON \
+                        , "Expected [%f,%f,%f,%f], got [%f,%f,%f,%f]", \
+                        first[0], first[1], first[2], first[3], \
+                        second[0], second[1], second[2], second[3])
