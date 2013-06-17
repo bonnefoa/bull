e4be08367c421ddccaf062aadbfdebaf08735e0c
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-06-17 17:33:44 +0200

    Remove useless framebuffer unbind

--- src/bl_program_texture.cpp
+++ src/bl_program_texture.cpp
@@ -43,9 +43,7 @@ void BlProgramTexture::init()
 
 void BlProgramTexture::displayTexture(GLuint textureId)
 {
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glViewport(0, 0, 512, 512);
-
         glUseProgram(programId);
 
         glActiveTexture(GL_TEXTURE4);
